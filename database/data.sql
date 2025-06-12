3e098583e0fca82347898eb78df9f2b5e70d424b
Ny Aro <nyarodina@gmail.com>
2025-06-12 19:12:07 +0300

    feat(database): add new table on schema & update mock data

--- database/data.sql
+++ database/data.sql
@@ -3,7 +3,13 @@ BEGIN;
 SET CONSTRAINTS ALL DEFERRED;
 
 -- Vider les tables existantes (l'ordre est important à cause des clés étrangères)
-TRUNCATE TABLE t_transaction, t_produit RESTART IDENTITY;
+TRUNCATE TABLE t_transaction, t_produit, t_user RESTART IDENTITY;
+
+-- Instructions d'insertion pour le jeu de données des utilisateurs
+INSERT INTO t_user (pk_id, psuedo, nom, prenom, mot_de_passe)
+VALUES 
+  (1, 'frodon', 'Baggins', 'Frodon', 'admin'),
+  (2, 'sam', 'Baggins', 'Samwise', 'user');
 
 -- Instructions d'insertion pour le jeu de données des produits
 -- Note: Les pk_id sont explicitement définis pour la clarté
@@ -21,17 +27,17 @@ VALUES
 
 -- Transactions d'entrée de stock pour initialiser les quantités
 -- Type 1 = Entrée de stock, Type 2 = Sortie de stock
-INSERT INTO t_transaction (pk_id, produit_id, quantite, type_transaction, date_ajout)
+INSERT INTO t_transaction (pk_id, produit_id, user_id, quantite, type_transaction, date_ajout)
 VALUES 
-  (1, 1, 20, 1, '2025-01-01 10:00:00'),
-  (2, 2, 250, 1, '2025-01-01 10:15:00'),
-  (3, 3, 2, 1, '2025-01-02 09:30:00'),
-  (4, 4, 15, 1, '2025-01-02 11:45:00'),
-  (5, 5, 30, 1, '2025-01-03 14:20:00'),
-  (6, 6, 5, 1, '2025-01-03 15:30:00'),
-  (7, 7, 25, 1, '2025-01-04 10:00:00'),
-  (8, 8, 10, 1, '2025-01-04 11:30:00'),
-  (9, 9, 18, 1, '2025-01-05 13:45:00');
+  (1, 1, 1, 20, 1, '2025-01-01 10:00:00'),
+  (2, 2, 1, 250, 1, '2025-01-01 10:15:00'),
+  (3, 3, 1, 2, 1, '2025-01-02 09:30:00'),
+  (4, 4, 1, 15, 1, '2025-01-02 11:45:00'),
+  (5, 5, 2, 30, 1, '2025-01-03 14:20:00'),
+  (6, 6, 2, 5, 1, '2025-01-03 15:30:00'),
+  (7, 7, 2, 25, 1, '2025-01-04 10:00:00'),
+  (8, 8, 2, 10, 1, '2025-01-04 11:30:00'),
+  (9, 9, 2, 18, 1, '2025-01-05 13:45:00');
 
 -- Pas besoin de réactiver les contraintes avec SET CONSTRAINTS
 -- car elles sont automatiquement réactivées à la fin de la transaction
--- database/schema.sql
+++ database/schema.sql
@@ -8,12 +8,23 @@ create table t_produit(
     quantite_en_stock int check ( quantite_en_stock >= 0 )
 );
 
+-- Table: user
+create table t_user(
+    pk_id serial primary key,
+    psuedo varchar not null,
+    nom varchar not null,
+    prenom varchar not null,
+    mot_de_passe varchar not null,
+);
+
 -- Table: gestion stock
 create table t_transaction(
     pk_id serial primary key,
     produit_id int not null,
+    user_id int not null,
     quantite int check ( quantite >= 0 ),
     date_ajout timestamp default current_timestamp,
     type_transaction int check ( type_transaction in (1, 2) ),
-    FOREIGN KEY (produit_id) REFERENCES t_produit(pk_id)
+    FOREIGN KEY (produit_id) REFERENCES t_produit(pk_id),
+    FOREIGN KEY (user_id) REFERENCES t_user(pk_id)
 );
